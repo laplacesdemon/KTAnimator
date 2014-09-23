6d3a3d7ef25a58a73de4d42837420c3a8287a559
Suleyman Melikoglu <suleymanmelikoglu@gmail.com>
2014-09-23 14:40:26 +0300

    example project works now

--- example/KTAnimatorExample/KTAnimatorExample/KTAppDelegate.m
+++ example/KTAnimatorExample/KTAnimatorExample/KTAppDelegate.m
@@ -7,13 +7,17 @@
 //
 
 #import "KTAppDelegate.h"
+#import "KTDemoViewController.h"
+
 
 @implementation KTAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+    
+    KTDemoViewController *vc = [[KTDemoViewController alloc] initWithNibName:@"KTDemoViewController" bundle:nil];
+    self.window.rootViewController = vc;
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
